f7ada4dd3264bad433f9413fa93fcf2d068d4171
Rodrigo Vargas <rodrigo.vargas@ennova-research.com>
2023-04-22 22:54:03 +0200

    convert into chatgpt

--- Discovery.md
+++ Discovery.md
@@ -1,15 +1,15 @@
 ---
-title: "Telemage"
-tagline: "A Telegram Bot for Generating Images via DALL-E"
+title: "TeleGPT"
+tagline: "A Telegram Interface to GPT"
 theme_color: "#800080"
-git: "https://github.com/xeust/telemage"
+git: "https://github.com/vargonis/telegpt"
 ---
 
-Your personal Telegram bot that uses DALL-E on Deta Space. Send a prompt via Telegram, and Telemage will use DALL-E to generate an image, store it in Drive and send it back to you.
+Your personal Telegram bot that uses GPT on Deta Space. Forked from [https://github.com/xeust/telespace](https://github.com/xeust/telespace)
 
 ### Setup
 
-1. Install the Telemage app on Deta Space.
+1. Install the TeleGPT app on Deta Space.
 2. In Telegram, open search, search for [Botfather](https://t.me/botfather) and add it as a contact. Talk to BotFather and create a Telegram bot (using the  `/newbot` command). BotFather will ask for a bot name and username, then it will give you a bot key.
 3. Paste the bot key from BotFather into the `TELEGRAM` configuration variable on Space (to get to Configuration, click the `...`, then 'Settings' then 'Configuration' from the App's Tile on your Canvas).
 4. Get an API key from [Open AI](https://beta.openai.com/account/api-keys). Input the Open AI API key into the `OPEN_AI` configuration variable on Space.
@@ -20,5 +20,6 @@ You are now ready to use your Bot.
 ### Use
 
 Add your bot (via the Bot's username from step 1) as a contact in Telegram. Start messaging it to interact with it. 
+Note that chat history is not submitted to the OpenAI API. Thus, each message is answered to in a fully independent manner.
 
-Visit [https://github.com/xeust/telespace](https://github.com/xeust/telespace) for the source.
+Visit [https://github.com/vargonis/telegpt](https://github.com/vargonis/telegpt) for the source.
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import base64
+# import base64
 import os
 
 import openai
 import requests
-from deta import Base, Drive
+from deta import Base #, Drive
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -18,54 +18,65 @@ class New_ID(BaseModel):
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="public"), name="public")
 
-PHOTOS = Drive("generations")
+# PHOTOS = Drive("generations")
 CONFIG = Base("config")
 
 BOT_KEY = os.getenv("TELEGRAM")
 OPEN_AI_KEY = os.getenv("OPEN_AI")
 BOT_URL = f"https://api.telegram.org/bot{BOT_KEY}"
-OPEN_AI_URL = "https://api.openai.com/v1/images/generations"
+# OPEN_AI_URL = "https://api.openai.com/v1/images/generations"
 
 env_error = (
-        not BOT_KEY
-        or BOT_KEY == "enter your key"
-        or not OPEN_AI_KEY
-        or OPEN_AI_KEY == "enter your key"
+    not BOT_KEY
+    or BOT_KEY == "enter your key"
+    or not OPEN_AI_KEY
+    or OPEN_AI_KEY == "enter your key"
 )
 
 openai.api_key = OPEN_AI_KEY
 
 
-def get_image_from_prompt(prompt):
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="512x512",
-        response_format="b64_json",
+def get_answer(message):
+    # response = openai.Image.create(
+    #     prompt=prompt,
+    #     n=1,
+    #     size="512x512",
+    #     response_format="b64_json",
+    # )
+    response = openai.ChatCompletion.create(
+        messages = [{
+            "role": "user",
+            "content": message,
+        }],
+        model = "gpt-3.5-turbo", # "gpt-4",
+        # max_tokens = 300,
+        n = 1,
+        # stop = ['.\n'],
+        temperature = 0.3,
+        # request_timeout = 15,
     )
-
     if "error" not in response:
-        return {
-            "b64img": response["data"][0]["b64_json"],  # type: ignore
-            "created": response["created"],  # type: ignore
-        }
-
-    return {"error": response["error"]["message"]}  # type: ignore
-
-
-def save_and_send_img(b64img, chat_id, prompt, timestamp):
-    image_data = base64.b64decode(b64img)
-    filename = f"{timestamp} - {prompt}.png"
-    PHOTOS.put(filename, image_data)
-    photo_payload = {"photo": image_data}
-    message_url = f"{BOT_URL}/sendPhoto?chat_id={chat_id}&caption={prompt}"
-    requests.post(message_url, files=photo_payload).json()
-    return {"chat_id": chat_id, "caption": prompt}
+        # return {
+        #     "b64img": response["data"][0]["b64_json"],  # type: ignore
+        #     "created": response["created"],  # type: ignore
+        # }
+        return {"message": response["choices"][0]["message"]["content"]}
+    return {"error": response["error"]["message"]}
 
 
-def send_error(chat_id, error_message):
+# def save_and_send_img(b64img, chat_id, prompt, timestamp):
+#     image_data = base64.b64decode(b64img)
+#     filename = f"{timestamp} - {prompt}.png"
+#     PHOTOS.put(filename, image_data)
+#     photo_payload = {"photo": image_data}
+#     message_url = f"{BOT_URL}/sendPhoto?chat_id={chat_id}&caption={prompt}"
+#     requests.post(message_url, files=photo_payload).json()
+#     return {"chat_id": chat_id, "caption": prompt}
+
+
+def send_message(chat_id, message):
     message_url = f"{BOT_URL}/sendMessage"
-    payload = {"text": error_message, "chat_id": chat_id}
+    payload = {"text": message, "chat_id": chat_id}
     return requests.post(message_url, json=payload).json()
 
 
@@ -90,6 +101,7 @@ def home():
         return HTMLResponse(home_template.render(status="READY"))
 
     return HTMLResponse(home_template.render(status="ERROR"))
+
 
 @app.get("/setup")
 def setup():
@@ -127,7 +139,7 @@ async def http_handler(request: Request):
 
     if "message" not in incoming_data:
         print(incoming_data)
-        return send_error(None, "Unknown error, lol, handling coming soon")
+        return send_message(None, "Unknown error, lol, handling coming soon")
 
     prompt = incoming_data["message"]["text"]
     chat_id = incoming_data["message"]["chat"]["id"]
@@ -144,31 +156,24 @@ async def http_handler(request: Request):
         return
 
     if prompt in ["/start", "/help"]:
-        response_text = (
-            "welcome to telemage. to generate an image with ai,"
-            " send me a prompt or phrase and i'll do some magic."
-        )
-        payload = {"text": response_text, "chat_id": chat_id}
+        payload = {"text": "Welcome to TeleGPT.", "chat_id": chat_id}
         message_url = f"{BOT_URL}/sendMessage"
         requests.post(message_url, json=payload).json()
         return
 
     if authorized_chat_ids is None or chat_id not in authorized_chat_ids.get("value"):  # type: ignore
-        payload = {"text": "you're not authorized. contact this bot's admin to authorize.", "chat_id": chat_id}
+        payload = {"text": "You're not authorized. Contact this bot's admin to authorize.", "chat_id": chat_id}
         message_url = f"{BOT_URL}/sendMessage"
         requests.post(message_url, json=payload).json()
         return
 
-    open_ai_resp = get_image_from_prompt(prompt)
-    if "b64img" in open_ai_resp:
-        return save_and_send_img(
-            open_ai_resp["b64img"], chat_id, prompt, open_ai_resp["created"]
-        )
-
+    open_ai_resp = get_answer(prompt)
     if "error" in open_ai_resp:
-        return send_error(chat_id, open_ai_resp["error"])
-
-    return send_error(chat_id, "Unknown error, lol, handling coming soon")
+        return send_message(chat_id, open_ai_resp["error"])
+    try:
+        return send_message(chat_id, open_ai_resp["message"])
+    except Exception as e:
+        return send_message(chat_id, f"Unknown error, sorry about that! Some details:\n{e}")
 
 
 @app.get("/set_webhook")
